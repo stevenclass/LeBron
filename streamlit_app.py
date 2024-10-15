3dd26394ec56a59dcd19caee086644b9b191e988
Shrey K. <87871039+shreykharbanda31@users.noreply.github.com>
2024-10-15 02:06:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,6 @@ if app_page == 'Visualization':
     df = st.session_state.df
     
     list_columns = df.columns
-    st.write(df.columns)
 
     values = st.multiselect("Select two variables:",list_columns,["FT%", "Pts"])
 
